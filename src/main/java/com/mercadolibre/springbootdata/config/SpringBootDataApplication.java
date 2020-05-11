b0d8d59d5f2c6bc862b5d9741392ee9d6e95637b
Néstor Flores <nestor.flores@mercadolibre.com>
2020-05-11 11:13:32 -0300

    Added project base structure

--- src/main/java/com/mercadolibre/springbootdata/config/SpringBootDataApplication.java
+++ src/main/java/com/mercadolibre/springbootdata/config/SpringBootDataApplication.java
@@ -1,4 +1,4 @@
-package com.mercadolibre.springbootdata;
+package com.mercadolibre.springbootdata.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
